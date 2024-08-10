a81e8234f22a3d86e6af02f1b4bdd47e7e9726f4
VictorCancino <juanvictorcancino@gmail.com>
2024-08-09 21:15:23 -0600

    Se creó el proyecto rust 'pitch'

---
+++ pitch/target/debug/deps/pitch-45141479247f5079.d
@@ -0,0 +1,5 @@
+C:\Users\juanv\proyectos_rust\hey_rust\pitch\target\debug\deps\libpitch-45141479247f5079.rmeta: src\main.rs
+
+C:\Users\juanv\proyectos_rust\hey_rust\pitch\target\debug\deps\pitch-45141479247f5079.d: src\main.rs
+
+src\main.rs:
---
+++ pitch/target/debug/deps/pitch-c86b6b7532c2e12a.d
@@ -0,0 +1,5 @@
+C:\Users\juanv\proyectos_rust\hey_rust\pitch\target\debug\deps\libpitch-c86b6b7532c2e12a.rmeta: src\main.rs
+
+C:\Users\juanv\proyectos_rust\hey_rust\pitch\target\debug\deps\pitch-c86b6b7532c2e12a.d: src\main.rs
+
+src\main.rs:
