4b81b80064b5f40783224a78e11d477b10f0312e
VictorCancino <juanvictorcancino@gmail.com>
2024-08-09 21:34:41 -0600

    Se eliminó del indice de proyectos del repositorio el proyecto 'pitch'

---
+++ target/debug/deps/hey_rustc.d
@@ -0,0 +1,5 @@
+C:\Users\juanv\proyectos_rust\hey_rust\target\debug\deps\hey_rustc.exe: hey_rustc\src\main.rs
+
+C:\Users\juanv\proyectos_rust\hey_rust\target\debug\deps\hey_rustc.d: hey_rustc\src\main.rs
+
+hey_rustc\src\main.rs:
---
+++ target/debug/hey_rustc.d
@@ -0,0 +1,1 @@
+C:\Users\juanv\proyectos_rust\hey_rust\target\debug\hey_rustc.exe: C:\Users\juanv\proyectos_rust\hey_rust\hey_rustc\src\main.rs
